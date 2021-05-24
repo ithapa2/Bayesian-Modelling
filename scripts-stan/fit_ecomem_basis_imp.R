32bb777a011e54915b2a0e7b450840df488be348
Andria Dawson <andria.dawson@gmail.com>
2021-05-24 08:53:56 -0600

    Modified fit_ecomem_basis_imp.R so easier to run with single covariate

--- scripts-stan/fit_ecomem_basis_imp.R
+++ scripts-stan/fit_ecomem_basis_imp.R
@@ -13,59 +13,60 @@ TOW = read.csv("data/TOW-BI.csv", header=TRUE)
 # CBS.sub = CBS[apply(CBS[,c('chron', 'tmin.may', 'pcp.aug', 'pdsi.sep')], 1, function(x) all(!is.na(x))),]
 # TOW.sub = TOW[apply(BTP[,c('chron', 'tmin.may', 'pcp.aug', 'pdsi.sep')], 1, function(x) all(!is.na(x))),]
 
-year_upper = min(max(BTP$year, na.rm=TRUE), max(CBS$year, na.rm=TRUE), max(TOW$year, na.rm=TRUE))
-year_lower = max(min(BTP$year, na.rm=TRUE), min(CBS$year, na.rm=TRUE), min(TOW$year, na.rm=TRUE))
+fire.raw = data.frame(year = seq(1650, 2017, by=1), fire = rep(0))
+fire.raw[which(fire.raw$year %in% c(1664, 1804, 1900)), 'fire'] = 1
 
+## find the start year and end year
+## for now require that chronologies and fire record all be the same length with no NA values
+## climate data of shorter length NA values will be imputed
+year_upper = min(max(BTP$year, na.rm=TRUE), max(CBS$year, na.rm=TRUE), max(TOW$year, na.rm=TRUE))
+year_lower = max(min(BTP$year, na.rm=TRUE), min(CBS$year, na.rm=TRUE), min(TOW$year, na.rm=TRUE), min(fire.raw$year[which(fire.raw$fire==1)]))
+
+# subset site and fire data to selected years
 BTP.sub = BTP[which(BTP$year %in% seq(year_lower, year_upper)),]
 CBS.sub = CBS[which(CBS$year %in% seq(year_lower, year_upper)),]
 TOW.sub = TOW[which(TOW$year %in% seq(year_lower, year_upper)),]
 
-fire.raw = data.frame(year = seq(1650, 2017, by=1), fire = rep(0))
-fire.raw[which(fire.raw$year %in% c(1664, 1804, 1900)), 'fire'] = 1
 fire.raw = fire.raw[which(fire.raw$year %in% BTP.sub$year),]
 
-Y  = as.matrix(data.frame(BTP.sub$chron, CBS.sub$chron, TOW.sub$chron))
-X1  = as.matrix(data.frame(BTP.sub$pcp.aug, CBS.sub$pcp.aug, TOW.sub$pcp.aug))
-X2  = as.matrix(data.frame(BTP.sub$pdsi.sep, CBS.sub$pdsi.sep, TOW.sub$pdsi.sep))
-d.raw  = as.matrix(data.frame(BTP.sub$tmin.may, CBS.sub$tmin.may, TOW.sub$tmin.may))
-fire.raw = as.matrix(fire.raw$fire)
+## define data objects
 
-# long.na = cbind(Y, fire.raw)
-# idx.long.na  = apply(long.na, 1, function(x) any(is.na(x)))
-# 
-# Y = Y[!idx.long.na,]
-# fire = fire.raw[!idx.long.na,]
-# 
-# N_years = nrow(Y)
-
-short.na = cbind(X1, X2, d.raw)
-idx.short.na  = which(apply(short.na, 1, function(x) any(is.na(x))))
-
-X1[idx.short.na,] = t(matrix(colMeans(X1, na.rm=TRUE)))[rep(1, length(idx.short.na)),]
-X2[idx.short.na,] = t(matrix(colMeans(X2, na.rm=TRUE)))[rep(1, length(idx.short.na)),]
-d.raw[idx.short.na,]  = t(matrix(colMeans(d.raw, na.rm=TRUE)))[rep(1, length(idx.short.na)),]
-
-d = d.raw
-fire = as.vector(fire.raw)
+# chronologies
+Y  = as.matrix(data.frame(BTP.sub$chron, CBS.sub$chron, TOW.sub$chron)) 
 
 Y = t(Y)
 
 N_years = ncol(Y)
 N_sites = nrow(Y)
 
-X1 = t(X1)
-X2 = t(X2)
 
-N_covars = 2
+# continuous memory var
+mem_var = 'tmin.may'
+lag = 6
+
+d = t(as.matrix(data.frame(BTP.sub[, mem_var], CBS.sub[, mem_var], TOW.sub[, mem_var])))
+
+# binary memory var
+fire.raw = as.matrix(fire.raw$fire)
+fire = as.vector(fire.raw)
+
+# covars
+covars = c('pcp.aug', 'pdsi.sep')
+N_covars = length(covars)
 
 X = array(NA, c(N_sites, N_years, N_covars))
+for (i in 1:N_covars){
+  X[,,i] = t(as.matrix(data.frame(BTP.sub[,covars[i]], CBS.sub[covars[i]], TOW.sub[covars[i]])))
+}
 
-X[,,1] = X1
-X[,,2] = X2
+idx.short.na  = which(apply(X, 2, function(x) any(is.na(x))))
+# idx.short.na.d  = which(apply(d, 1, function(x) any(is.na(x))))
 
-d = t(d)
+for (i in 1:N_covars){
+  X[,idx.short.na,i] =  matrix(rowMeans(X[,,i], na.rm=TRUE))[,rep(1, length(idx.short.na))]
+}
 
-lag = 6
+d[idx.short.na,]  = matrix(rowMeans(d, na.rm=TRUE))[,rep(1, length(idx.short.na))]
 
 
 X_nmiss = length(idx.short.na)
@@ -152,6 +153,56 @@ dat = list(N_years = N_years,
            X_index = X_index,
            d_index = d_index)
 
+saveRDS(dat, 'scripts-stan/output/data_ecomem_basis_imp_test.RDS')
+
+# 
+# #######################################################################################
+# ## inits
+# #######################################################################################
+# sigma_eta = 0.1;
+# 
+# library(mvtnorm)
+# 
+# eta = rnorm(6, 0, 1)
+# 
+# eta * rmvnorm(1, rep(0, n_basis), sigma_eta^2 * S_inv)
+# 
+# B_eta = eta * rmvnorm(1, rep(0, n_basis), sigma_eta^2 * S_inv)
+# eta * rmvnorm(1, rep(0, n_basis), sigma_eta^2 * S_inv);
+# 
+# exp(B_eta)/sum(exp(B_eta))
+# 
+# tau_fire = 0.5
+# sigma = abs(rcauchy(3, 0.01, 0.2))
+# sigma_alpha = 0.2
+# u = matrix(0, nrow=N_sites, ncol=N_years)
+# beta = rep(0, N_covars)
+# gamma0 = 0
+# gamma1 = 0 
+# gamma2 = 0
+# 
+# Sigma = diag(N_covars + 2)
+# mu = matrix(0, nrow=N_sites, ncol=N_covars+2)
+# 
+# X1_imp = matrix(rep(rowMeans(X1), times = X_nmiss), nrow=N_sites, ncol=X_nmiss)
+# X2_imp = matrix(rep(rowMeans(X2), times = X_nmiss), nrow=N_sites, ncol=X_nmiss)
+# d_imp  = matrix(rep(rowMeans(d), times = X_nmiss), nrow=N_sites, ncol=X_nmiss) 
+# 
+# inits = list(list(tau_fire = tau_fire,
+#                  sigma = sigma,
+#                  sigma_alpha = sigma_alpha,
+#                  u = u,
+#                  beta = beta,
+#                  gamma0 = gamma0,
+#                  gamma1 = gamma1,
+#                  gamma2 = gamma2,
+#                  eta = eta,
+#                  Sigma = Sigma,
+#                  mu = mu,
+#                  X1_imp = X1_imp,
+#                  X2_imp = X2_imp,
+#                  d_imp = d_imp))
+    
 #######################################################################################
 ## sampling
 #######################################################################################
@@ -161,13 +212,14 @@ N_iter = 500
 #rstan_options(auto_write = TRUE)
 #options(mc.cores = parallel::detectCores())
 
-sm<-stan_model("scripts-stan/ecomem_basis_imp.stan")
+sm<-stan_model("scripts-stan/ecomem_basis_imp_test.stan")
 
 fit<-sampling(sm,
               data=dat,
               iter=N_iter,
               chains = 1, 
-              cores = 1)#,control = list(adapt_delta=0.95))
+              cores = 1)#,
+              #init = inits)#,control = list(adapt_delta=0.95))
 
-saveRDS(fit, 'scripts-stan/output/fit_ecomem_basis_imp.RDS')
+saveRDS(fit, 'scripts-stan/output/fit_ecomem_basis_imp_test.RDS')
 
