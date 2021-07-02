af4d7ccce04df4e97ef81545b15e50ce99b28a05
Andria Dawson <andria.dawson@gmail.com>
2021-07-02 12:27:44 -0600

    update plotting script to include outbreak

--- scripts-stan/common/plot_ecomem_basis_imp.R
+++ scripts-stan/common/plot_ecomem_basis_imp.R
@@ -26,7 +26,7 @@ spherical <- function(x, phi) {
 # 
 # suff = 'imp'
 
-fit = readRDS(paste0('scripts-stan/output/fit_ecomem_basis_imp_', suffix, '.RDS'))
+fit = readRDS(paste0(path_output, '/fit_ecomem_basis_imp_', suffix, '.RDS'))
 
 plot(fit)
 
@@ -40,7 +40,7 @@ names(post)
 ## load data
 #######################################################################################
 
-dat = readRDS(paste0('scripts-stan/output/data_ecomem_basis_imp_', suffix, '.RDS'))
+dat = readRDS(paste0(path_output, '/data_ecomem_basis_imp_', suffix, '.RDS'))
 
 n_basis = dat$n_basis
 N_sites = dat$N_sites
@@ -49,6 +49,10 @@ lag = dat$lag
 #######################################################################################
 ## plot output
 #######################################################################################
+
+if (!dir.exists(path_figures)){
+  dir.create(path_figures)
+}
 
 ## fire memory
 
@@ -76,11 +80,45 @@ ggplot(data=mem.fire.dat) +
   geom_ribbon(aes(x=x, ymin=q5, ymax=q95), fill='dodgerblue', alpha=0.4) +
   theme_bw() +
   theme(text = element_text(size=22)) + 
- # xlab("Lag") +
+  # xlab("Lag") +
   ylab("Fire Antecedent Weight") +
   scale_x_continuous(name="Lag", breaks=seq(0, 6))
-ggsave(paste0('scripts-stan/figures/antecedent-weights-fire_', suffix, '.png'))
+ggsave(paste0(path_figures, '/antecedent-weights-fire_', suffix, '.png'))
 
+
+if (include_outbreak){
+  ## insect memory
+  
+  hist(post$tau_outbreak)
+  
+  phi_insect = 1/post$tau_outbreak
+  phi_insect.mean = mean(phi_insect)
+  
+  x = seq(1e-6, lag, by=0.1)
+  mem.insect = unlist(lapply(x, function(x){spherical(x, phi_insect.mean)}))
+  
+  plot(x, mem.insect, xlab="Year", ylab="Antecedent weight")
+  
+  mem.insect.iter = matrix(NA, nrow=length(phi_insect), ncol=length(x))
+  for (i in 1:length(phi_insect)){
+    print(i)
+    mem.insect.iter[i,] = unlist(lapply(x, function(x){spherical(x, phi_insect[i])}))
+  }
+  
+  mem.insect.quants = apply(mem.insect.iter, 2, function(x) quantile(x, c(0.05, 0.5, 0.95)))
+  mem.insect.dat = data.frame(x=x, t(mem.insect.quants))
+  colnames(mem.insect.dat) = c('x', 'q5', 'q50', 'q95')
+  
+  ggplot(data=mem.insect.dat) + 
+    geom_line(aes(x=x, y=q50), lwd=2) +
+    geom_ribbon(aes(x=x, ymin=q5, ymax=q95), fill='dodgerblue', alpha=0.4) +
+    theme_bw() +
+    theme(text = element_text(size=22)) + 
+    # xlab("Lag") +
+    ylab("Fire Antecedent Weight") +
+    scale_x_continuous(name="Lag", breaks=seq(0, 6))
+  ggsave(paste0(path_figures, '/antecedent-weights-insect_', suffix, '.png'))
+}
 
 # mem.fire.melt = melt(mem.fire.iter)
 # colnames(mem.fire.melt) = c('iter', 'lag', 'value')
@@ -89,27 +127,57 @@ ggsave(paste0('scripts-stan/figures/antecedent-weights-fire_', suffix, '.png'))
 
 gamma_idx = which(substr(names(post), 1, 5) == "gamma")
 
-gamma = matrix(unlist(post[gamma_idx]), ncol = length(gamma_idx), byrow = FALSE)
-colnames(gamma) = paste0('gamma', seq(0,ncol(gamma)-1))
+
+gamma_dim = rep(NA, length(gamma_idx))
+gamma_names = c()
+for (i in 1:length(gamma_idx)){
+  gamma_dim[i] = ncol(data.frame(post[gamma_idx[i]][[1]] ))
+  gamma_name = names(post[gamma_idx[i]])
+  if (gamma_dim[i] == 1) {
+    gamma_name_rep = gamma_name
+  } else {
+    gamma_name_rep = paste0(gamma_name, '_', seq(1, gamma_dim[i]))
+    #gamma_name_rep = rep(gamma_name, gamma_dim[i])
+    
+  }
+  gamma_names = c(gamma_names, gamma_name_rep)
+}
+
+#gamma = matrix(unlist(post[gamma_idx]), ncol = length(gamma_idx), byrow = FALSE)
+gamma = matrix(unlist(post[gamma_idx]), ncol = sum(gamma_dim), byrow = FALSE)
+
+
+#colnames(gamma) = paste0('gamma', seq(0,ncol(gamma)-1))
+colnames(gamma) = gamma_names
 gamma.melt = melt(gamma)
 
-ggplot(data=subset(gamma.melt, Var2 %in% c('gamma0', 'gamma1'))) +
+gamma.melt$type = substr(gamma.melt$Var2, 1, 6)
+
+ggplot(data=subset(gamma.melt, type %in% c('gamma0', 'gamma1'))) +
   geom_density(aes(x=value), lwd=2, fill="dodgerblue", alpha=0.2) +
   geom_rug(aes(x =value, y = 0), position = position_jitter(height = 0)) +
   theme_bw() +
-  theme(text = element_text(size=22)) +
+  theme(text = element_text(size=14)) +
   facet_wrap(Var2~., scales="free_x")
-ggsave(paste0('scripts-stan/figures/gamma-small-post-density_', suffix, '.png'))
+ggsave(paste0(path_figures, '/gamma-small-post-density_', suffix, '.png'))
 
 
-ggplot(data=subset(gamma.melt, Var2 %in% c('gamma0', 'gamma1'))) +
+ggplot(data=subset(gamma.melt, type %in% c('gamma0', 'gamma1'))) +
   geom_line(aes(x=Var1, y=value), lwd=2) +
   theme_bw() +
-  theme(text = element_text(size=22)) +
+  theme(text = element_text(size=14)) +
   facet_grid(Var2~., scales="free_y") +
   xlab("Iteration")
-ggsave(paste0('scripts-stan/figures/gamma-small-trace_', suffix, '.png'))
+ggsave(paste0(path_figures, '/gamma-small-trace_', suffix, '.png'))
 
+
+ggplot(data=subset(gamma.melt, type %in% c('gamma2'))) +
+  geom_line(aes(x=Var1, y=value), lwd=2) +
+  theme_bw() +
+  theme(text = element_text(size=14)) +
+  facet_grid(Var2~., scales="free_y") +
+  xlab("Iteration")
+ggsave(paste0(path_figures, '/gamma2-trace_', suffix, '.png'))
 
 ggplot(data=subset(gamma.melt, Var2 %in% c('gamma2'))) +
   geom_density(aes(x=value), lwd=2, fill="dodgerblue", alpha=0.2) +
@@ -117,18 +185,21 @@ ggplot(data=subset(gamma.melt, Var2 %in% c('gamma2'))) +
   theme_bw() +
   theme(text = element_text(size=22)) +
   facet_wrap(Var2~., scales="free_x")
-ggsave(paste0('scripts-stan/figures/gamma2-post-density_', suffix, '.png'))
+ggsave(paste0(path_figures, '/gamma2-post-density_', suffix, '.png'))
 
 #quantile(gamma1, c(0.10, 0.5, 0.90))
 gamma.quants = apply(gamma, 2, quantile, c(0.05, 0.5, 0.95))
 gamma.quants
-coef.dat = data.frame(par=paste0('gamma', seq(0,ncol(gamma)-1)), t(gamma.quants))
+# coef.dat = data.frame(par=paste0('gamma', seq(0,ncol(gamma)-1)), t(gamma.quants))
+coef.dat = data.frame(par=colnames(gamma.quants), t(gamma.quants))
 
+beta_idx = which(substr(names(post), 1, 5) == "beta")
+beta_dim = ncol(data.frame(post[beta_idx][[1]]))
 
-beta_idx = which(substr(names(post), 1, 4) == "beta")
+beta_names = paste0('beta', seq(0, beta_dim-1))
 
-beta = matrix(unlist(post[beta_idx]), ncol = length(beta_idx), byrow = FALSE)
-colnames(beta) = paste0('beta', seq(0,ncol(beta)-1))
+beta = matrix(unlist(post[beta_idx]), ncol = beta_dim, byrow = FALSE)
+colnames(beta) = beta_names
 
 beta.quants = apply(beta, 2, quantile, c(0.05, 0.5, 0.95))
 beta.quants
@@ -172,7 +243,7 @@ ggplot(data=coef.dat) +
 #   ylab('') +
 #   geom_vline(xintercept=0, colour="red", linetype="dashed") #+
 #   #scale_x_continuous(name="Value", limits=c(-0.008, 0)) 
-# ggsave(paste0('scripts-stan/figures/gamma-credible_', suff, '.png'))
+# ggsave(paste0(path_figures, '/gamma-credible_', suff, '.png'))
 
 
 # ggplot(data=coef.dat[c(3),]) +
@@ -186,7 +257,7 @@ ggplot(data=coef.dat) +
 #   ylab('') +
 #   geom_vline(xintercept=0, colour="red", linetype="dashed") #+
 # #scale_x_continuous(name="Value", limits=c(-0.008, 0)) 
-# ggsave(paste0('scripts-stan/figures/gamma-credible_', suff, '.png'))
+# ggsave(paste0(path_figures, '/gamma-credible_', suff, '.png'))
 
 
 ggplot(data=coef.dat[c(4,5),]) +
@@ -200,7 +271,7 @@ ggplot(data=coef.dat[c(4,5),]) +
   ylab('') +
   geom_vline(xintercept=0, colour="red", linetype="dashed") +
   scale_x_continuous(name="Value", limits=c(-0.008, 0)) 
-ggsave(paste0('scripts-stan/figures/beta-credible_', suffix, '.png'))
+ggsave(paste0(path_figures, '/beta-credible_', suffix, '.png'))
 
 
 quantile(post$sigma, c(0.10, 0.5, 0.90))
@@ -226,9 +297,9 @@ ggplot(data=w.dat) +
   geom_ribbon(aes(x=vals, ymin=q5, ymax=q95), fill='dodgerblue', alpha=0.5) +
   theme_bw() +
   theme(text = element_text(size=22)) +
-  ylab("May Mininum Temperature \n Antecedent Weight") +
+  ylab("Memory Var \n Antecedent Weight") +
   scale_x_continuous(name="Lag", breaks=seq(0, 6))
-ggsave(paste0('scripts-stan/figures/antecedent-weights-tmin-may_', suffix, '.png'))
+ggsave(paste0(path_figures, '/antecedent-weights-memory-var_', suffix, '.png'))
 
 
 
@@ -238,3 +309,4 @@ plot(seq(0, lag), w_mean)
 
 hist(post$sigma)
 hist(post$sigma_alpha)
+
