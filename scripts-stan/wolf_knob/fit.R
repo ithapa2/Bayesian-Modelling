17581697a910e21a31dcaa68c0d46127454db87a
Andria Dawson <andria.dawson@gmail.com>
2021-07-06 22:15:22 -0600

    Updating wolf knob plot and fit scripts (minor)

--- scripts-stan/wolf_knob/fit.R
+++ scripts-stan/wolf_knob/fit.R
@@ -7,7 +7,10 @@
 sites = c('PSME')
 fnames = c("data/WOLF_PSME_FINAL-std-all.csv")
 
-suffix = 'stream-insect'
+# suffix = 'fire-stream-logy'
+#suffix = 'insect-stream-logy'
+# suffix = 'fire-stream-logy'
+suffix = 'insect-stream-logy_test'
 
 path_output = 'scripts-stan/wolf_knob/output'
 path_figures = 'scripts-stan/wolf_knob/figures'
@@ -44,25 +47,35 @@ lag = 6
 #covars = c('tmax.paug', 'pdsi.poct')
 covars = c('tmax.lsum', 'swe.yel')
 
+include_outbreak = 1
+include_fire     = 0
+
 #######################################################################################
 ## prepare data for stan model
 #######################################################################################
 
-fire.raw = data.frame(year = seq(736, 2017, by=1), fire = rep(0))
+fire.raw = data.frame(year = seq(220, 2017, by=1), fire = rep(0))
 fire.raw[which(fire.raw$year %in% c(388, 402, 432, 663, 690, 957, 964, 1023, 1056, 1110, 1144, 1148, 1180, 1190, 1213, 1217, 1277, 1278, 
                                     1296, 1299, 1306, 1317, 1325, 1355, 1361, 1388, 1407, 1437, 1439, 1446, 1468, 
                                     1489, 1492, 1499, 1528, 1531, 1532, 1538, 1543, 1551, 1556, 1561, 1571, 1576, 
                                     1580, 1584, 1590, 1593, 1598, 1600, 1626, 1631, 1671, 1703, 1705, 1742, 1765, 
                                     1800, 1836, 1842, 1894, 1949, 1956, 1966, 1997)), 'fire'] = 1
 
-include_outbreak = 0
-
 insect = read.csv('data/JPTD-budworm-outbreak.csv', header=TRUE)
 insect$bool = insect$percent
-insect$bool[which(insect$percent>35)] = 1
-insect$bool[which(insect$percent<=35)] = 0
+insect$bool[which(insect$percent>60)] = 1
+insect$bool[which(insect$percent<=60)] = 0
+
+if (xor(include_outbreak, include_fire)){
+  model_name = 'ecomem_basis_imp_logy.stan'
+}
+
+if ((include_outbreak)&(include_fire)){
+  model_name = 'ecomem_basis_imp_logy_2dmem.stan'
+}
+
 
 #model_name = 'ecomem_basis_imp.stan'
-model_name = 'ecomem_basis_imp_insect.stan'
+# model_name = 'ecomem_basis_imp.stan'
 
-source("scripts-stan/common/fit_ecomem_basis_imp.R")
+source("scripts-stan/common/fit_ecomem_basis_imp_ndmem.R")
--- scripts-stan/wolf_knob/plot.R
+++ scripts-stan/wolf_knob/plot.R
@@ -1,5 +1,5 @@
 
-suffix = 'WK'
+suffix = 'insect-stream-logy_test'
 
 source("scripts-stan/common/plot_ecomem_basis_imp.R")
 
